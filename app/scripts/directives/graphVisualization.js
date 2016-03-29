3d1633d0d55a157cde33f603299c21be10f9a2fc
Maria Akritidou <mariaak83@gmail.com>
2016-03-29 23:46:23 +0300

    Add arrows & labels.

--- app/scripts/directives/graphVisualization.js
+++ app/scripts/directives/graphVisualization.js
@@ -9,42 +9,75 @@
           val: '='
         },
         link: function(scope, element, attrs) {
-          var width = 800,
-            height = 500,
-            nodes,
-            links;
+          var width = 960,
+              height = 500,
+              root;
 
           var force = d3.layout.force()
-              .linkDistance(120)
-              .charge(-120)
-              .gravity(.05)
+              .linkDistance(150)
+              .charge([-500])
+              .theta(0.1)
+              .gravity(0.05)
               .size([width, height])
               .on("tick", tick);
 
-          var svg = d3.select("#content").append("svg")
+          var svg = d3.select("body").append("svg")
               .attr("width", width)
               .attr("height", height);
 
           var link = svg.selectAll(".link"),
-            node = svg.selectAll(".node");
+              node = svg.selectAll(".node"),
+              linkpath = svg.selectAll(".linkpath"),
+              linklabel = svg.selectAll(".linklabel");
+
+          // build the arrow.
+          svg.append('defs').append('marker')
+        .attr({'id':'arrowhead',
+               'viewBox':'-0 -5 10 10',
+               'refX':20,
+               'refY':0,
+               //'markerUnits':'strokeWidth',
+               'orient':'auto',
+               'markerWidth':10,
+               'markerHeight':10,
+               'xoverflow':'visible'})
+        .append('svg:path')
+            .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+            .attr('fill', '#ccc')
+            .attr('stroke','#ccc');
 
           function update() {
+
+            var nodes = flatten(root),
+                links = d3.layout.tree().links(nodes);
+
+            // for (var l in links) {
+            //   if (link.direction == 'self') {
+            //     d._target = d.target;
+            //     d.target = d.source;
+            //     d.source = d._target;
+            //   }
+            // }
+
             // Restart the force layout.
             force
                 .nodes(nodes)
                 .links(links)
                 .start();
 
-            // Update links.
-            link = link.data(links, function(d) { return d.target.id; });
+            link = link.data(links, function(d) { console.info('linkdata'+d.target.id);return d.target.id; });
 
             link.exit().remove();
 
-            link.enter().insert("line", ".node")
-                .attr("class", "link");
-
+            link.enter().append("line")
+              .attr("id",function(d,i) {console.info('link'+i);return 'link'+i})
+              .attr('marker-end','url(#arrowhead)')
+              .attr("class", "link")
+              .style("stroke","#ccc")
+              .style("pointer-events", "none");
+console.info('nodes');
             // Update nodes.
-            node = node.data(nodes, function(d) { return d.id; });
+            node = node.data(nodes, function(d) { console.info('node'+d);return d.id; });
 
             node.exit().remove();
 
@@ -54,7 +87,7 @@
                 .call(force.drag);
 
             nodeEnter.append("circle")
-                .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 5.5; });
+                .attr("r", function(d) { return d.type == 'dependency' ? 4.5 : 8; });
 
             nodeEnter.append("text")
                 .attr("dy", ".35em")
@@ -62,15 +95,68 @@
 
             node.select("circle")
                 .style("fill", color);
+
+            linkpath = linkpath.data(links, function(d) { console.info('path'+d.target.id);return d.target.id; });
+
+            linkpath.exit().remove();
+
+            // TODO
+            linkpath.enter()
+                .append('path')
+                .attr({'d': function(d) {return 'M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y},
+                       'class':'linkpath',
+                       'fill-opacity':0,
+                       'stroke-opacity':0,
+                       'fill':'blue',
+                       'stroke':'red',
+                       'id':function(d,i) {console.info('linkpath'+i);return 'linkpath'+i}})
+                .style("pointer-events", "none");
+
+            linklabel = linklabel.data(links);
+
+            linklabel.exit().remove();
+
+            linklabel.enter()
+                .append('text')
+                .style("pointer-events", "none")
+                .attr({'class':'linklabel',
+                       'id':function(d,i){return 'linklabel'+i},
+                       'dx':80,
+                       'dy':0,
+                       'font-size':10,
+                       'fill':'#aaa'});
+
+            linklabel.append('textPath')
+                .attr('xlink:href',function(d,i) {return '#linkpath'+i})
+                .style("pointer-events", "none")
+                .text(function(d,i){return "label" + i});
           }
 
           function tick() {
-            link.attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
+            link.attr({"x1": function(d){return d.source.x;},
+                    "y1": function(d){return d.source.y;},
+                    "x2": function(d){return d.target.x;},
+                    "y2": function(d){return d.target.y;}
+            });
 
             node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+            linkpath.attr('d', function(d) {
+              var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+              return path;
+            });
+
+            linklabel.attr('transform',function(d,i){
+                if (d.target.x<d.source.x){
+                    var bbox = this.getBBox();
+                    var rx = bbox.x+bbox.width/2;
+                    var ry = bbox.y+bbox.height/2;
+                    return 'rotate(180 '+rx+' '+ry+')';
+                    }
+                else {
+                    return 'rotate(0)';
+                    }
+            });
           }
 
           function color(d) {
@@ -92,13 +178,26 @@
             update();
           }
 
+          // Returns a list of all nodes under the root.
+          function flatten(root) {
+            var nodes = [], i = 0;
+
+            function recurse(node) {
+              if (node.children) node.children.forEach(recurse);
+              if (!node.id) node.id = ++i;
+              nodes.push(node);
+            }
+
+            recurse(root);
+            return nodes;
+          }
+
           scope.$watch('val', function(graphData, graphDataOld) {
             if (graphData === graphDataOld) {
               return;
             }
 
-            nodes = graphData.nodes;
-            links = graphData.links;
+            root = graphData;
             update();
           });
         }
