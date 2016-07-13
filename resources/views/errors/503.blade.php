4d164727928bc6f2f6927990e1d11b9c89bc6af5
Maria Akritidou <mariaak83@gmail.com>
2016-07-13 15:50:10 +0300

    Customize error page.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+@extends('layouts.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
-            </div>
+@section('content')
+<div class="container-fluid main-content">
+    <div class="row widget-container fluid-height">
+      <div class="col-md-12">
+        <div class="widget-content padded text-center">
+            <h1 class="text-danger">Be right back.</h1>
         </div>
-    </body>
-</html>
+      </div>
+    </div>
+</div>
+@endsection
