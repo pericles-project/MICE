38ddfee49623c8dc9a1e19703b793ae048f443bc
Maria Akritidou <maria@dotsoft.gr>
2016-11-30 17:55:17 +0000

    Add mice logo.

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -7,7 +7,7 @@
       <div class="row widget-container fluid-height">
         <div class="col-md-12">
           <div class="widget-content padded text-center">
-              <h1 class="text-danger">No input parameters specified</h1>
+              <h1>Getting Started</h1>
               <p>&nbsp;</p>
               <p>Please check our <a href="https://github.com/pericles-project/MICE/wiki" target="_blank">Quick Start Guide</a> to read instructions on how to use the tool.</p>
               <p>&nbsp;</p>
